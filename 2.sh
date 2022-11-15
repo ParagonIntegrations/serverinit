d612d5ca5b06446408eec31dd37e308eeecc9a24
Nelis Volschenk <31322360+NelisVolschenk@users.noreply.github.com>
2022-11-15 11:53:59 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,13 +5,11 @@ set -euo pipefail
 apt update -y
 apt upgrade -y
 
-#install docker and docker-compose
+#install docker, compose and give user permissions
 apt install apt-transport-https ca-certificates curl software-properties-common -y
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update -y
 apt-cache policy docker-ce
-apt install docker-ce -y
-apt install docker-compose -y
-usermod -aG docker nelis
+apt install install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+usermod -aG docker ${USER}
