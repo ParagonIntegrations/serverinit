2b85f5bf9218c528997052b71b20f4cd2d39e704
Nelis Volschenk <31322360+NelisVolschenk@users.noreply.github.com>
2022-11-15 12:09:53 +0200

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -11,5 +11,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update -y
 apt-cache policy docker-ce
-apt install install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 usermod -aG docker ${USER}
